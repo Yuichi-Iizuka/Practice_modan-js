47e6a66b41dc22ec37ed7dbdc644217c7071eed0
Yu <75148506+Yuichi-Iizuka@users.noreply.github.com>
2023-06-28 12:43:43 +0000

    Update 13 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import "./styles.css";
+/**
+ * const let 
+ * 
+ * 
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+/**
+ * テンプレート文字列
+ * 
+ */
+
+const name = "poko";
+const age = 28;
+// my name is 
+
+// 過去
+const message1  = "私の名前は" + name + "です。年齢は" + age + "です";
+console.log(message1);
